d0b94fa9d3ba55beecc7aa31075257fe97f6038b
Diego Rojas <darguelles.rojas91@gmail.com>
2020-08-14 01:07:49 -0300

    Creando clase Gerente

---
+++ src/bytebank/Gerente.java
@@ -0,0 +1,55 @@
+package bytebank;
+
+public class Gerente {
+
+    private String nombre;
+    private String documento;
+    private double salario;
+    private int tipo;
+    private int clave;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getBonificacion() {
+        return this.salario;
+    }
+
+    public boolean autentica(int senha) {
+        if(this.clave == senha) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
